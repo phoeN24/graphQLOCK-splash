a154c8f535193403623cf04e0601227f0751ca58
phoeN24 <phoenix@kilodelta.com>
2022-03-08 12:25:23 -0800

    gitHib button partially styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ const navbar = () =>{
   <ul id="navbar">
     <li>
       <img id="logo" src={logo}/>
-      <b id="title">graphQLock</b>
+      <b id="title">GraphQLock</b>
     </li>
     <li id="github" >
-      <a href="https://github.com/oslabs-beta/graphQLock" target="_blank">Github</a>
+      <a href="https://github.com/oslabs-beta/graphQLock" target="_blank" class="button">Github</a>
     </li>
   </ul>
   );
