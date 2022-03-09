fe9414b946637c6934b117cb18dad6a326adfbe4
phoeN24 <phoenix@kilodelta.com>
2022-03-09 10:06:21 -0800

    fixed about info

--- src/components/graphqlock.js
+++ src/components/graphqlock.js
@@ -4,12 +4,12 @@ const AboutGL = () => {
   return (
     <div id="about">
       <p>
-       GraphQLOCK is a server-side NPM middleware library package that provides authentication and authorization for Express GraphQL
+       GraphQLock is a server-side NPM middleware library package that provides authentication and authorization for Express GraphQL
        servers based on user defined roles and operations, whilst allowing full access and control to security data.
        
       </p>
       <p> 
-        GraphQLOCK also provides a backbone MongoDB model for the client to persist refresh tokens for continual authorization
+        GraphQLock also provides a backbone MongoDB model for the client to persist refresh tokens for continual authorization
         and authentication with frictionless set up through the use of env file and environment variables.
       </p>
     </div>
