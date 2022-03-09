30bded50fd6d4ff584aeff5af9bfe4cc8292a2a6
phoeN24 <phoenix@kilodelta.com>
2022-03-08 19:11:05 -0800

    just need to style github and linkden links

--- src/components/theTeam.js
+++ src/components/theTeam.js
@@ -3,20 +3,20 @@ import React from 'react';
 const Team = () => {
   return (
     <ul class="team"> 
-      <div id="charlie">
-        <a class="github" href="https://github.com/CharlieHuangDev" target="_blank">CharlieHuangDev</a>
+      <div class="dev">
+        <a class="github" href="https://github.com/CharlieHuangDev" target="_blank">CharlieHuangDev</a><br/>
         <a class="linkden" href="https://github.com/CharlieHuangDev" target="_blank">put Charlie linkden</a>
       </div>
-      <div id="drake">
-        <a class="github" href="https://github.com/DRWilliams527" target="_blank">DRWilliams527</a>
+      <div class="dev">
+        <a class="github" href="https://github.com/DRWilliams527" target="_blank">DRWilliams527</a><br/>
         <a class="linkden" href="https://github.com/CharlieHuangDev" target="_blank">put Drake linkden</a>
       </div>
-      <div id="jiwon">
-        <a class="github" href="https://github.com/jchung07" target="_blank">jchung07</a>
+      <div class="dev">
+        <a class="github" href="https://github.com/jchung07" target="_blank">jchung07</a><br/>
         <a class="linkden" href="https://www.linkedin.com/in/jchung07/" target="_blank">Jiwon Chung</a>
       </div>
-      <div id="phoenix">
-        <a class="github" href="https://github.com/phoeN24" target="_blank">phoeN24</a>
+      <div class="dev">
+        <a class="github" href="https://github.com/phoeN24" target="_blank">phoeN24</a><br/>
         <a class="linkden" href="https://www.linkedin.com/in/phoenixnewman/" target="_blank">Phoenix Newman</a>
       </div>
     </ul>
