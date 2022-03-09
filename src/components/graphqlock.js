79e7b9d4e169a836082d40d0d55e0a2866d89897
phoeN24 <phoenix@kilodelta.com>
2022-03-08 19:54:00 -0800

    ready to deploy

--- src/components/graphqlock.js
+++ src/components/graphqlock.js
@@ -6,8 +6,11 @@ const AboutGL = () => {
       <p>
        GraphQLOCK is a server-side NPM middleware library package that provides authentication and authorization for Express GraphQL
        servers based on user defined roles and operations, whilst allowing full access and control to security data.
-       GraphQLOCK also provides a backbone MongoDB model for the client to persist refresh tokens for continual authorization
-       and authentication with frictionless step up through the use of env file and environment variables.
+       
+      </p>
+      <p> 
+        GraphQLOCK also provides a backbone MongoDB model for the client to persist refresh tokens for continual authorization
+        and authentication with frictionless step up through the use of env file and environment variables.
       </p>
     </div>
   )
--- src/components/theTeam.js
+++ src/components/theTeam.js
@@ -4,20 +4,20 @@ const Team = () => {
   return (
     <ul class="team"> 
       <div class="dev">
-        <a class="github" href="https://github.com/CharlieHuangDev" target="_blank">CharlieHuangDev</a><br/>
-        <a class="linkden" href="https://github.com/CharlieHuangDev" target="_blank">put Charlie linkden</a>
+        <a class="linkden" href="https://www.linkedin.com/in/huangcharlie/" target="_blank">Charlie Huang</a><br/>
+        <a class="github" href="https://github.com/CharlieHuangDev" target="_blank">@CharlieHuangDev</a>
       </div>
       <div class="dev">
-        <a class="github" href="https://github.com/DRWilliams527" target="_blank">DRWilliams527</a><br/>
-        <a class="linkden" href="https://github.com/CharlieHuangDev" target="_blank">put Drake linkden</a>
+        <a class="linkden" href="https://www.linkedin.com/in/drake-williams-7b8b1262/" target="_blank">Drake williams</a><br/>
+        <a class="github" href="https://github.com/DRWilliams527" target="_blank">@DRWilliams527</a>
       </div>
       <div class="dev">
-        <a class="github" href="https://github.com/jchung07" target="_blank">jchung07</a><br/>
-        <a class="linkden" href="https://www.linkedin.com/in/jchung07/" target="_blank">Jiwon Chung</a>
+        <a class="linkden" href="https://www.linkedin.com/in/jchung07/" target="_blank">Jiwon Chung</a><br/>
+        <a class="github" href="https://github.com/jchung07" target="_blank">@jchung07</a>
       </div>
       <div class="dev">
-        <a class="github" href="https://github.com/phoeN24" target="_blank">phoeN24</a><br/>
-        <a class="linkden" href="https://www.linkedin.com/in/phoenixnewman/" target="_blank">Phoenix Newman</a>
+        <a class="linkden" href="https://www.linkedin.com/in/phoenixnewman/" target="_blank">Phoenix Newman</a><br/>
+        <a class="github" href="https://github.com/phoeN24" target="_blank">@phoeN24</a>
       </div>
     </ul>
     
