55d93b8bb8409a646a14b287d2e258293db57b79
phoeN24 <phoenix@kilodelta.com>
2022-03-09 14:27:25 -0800

    added link to medium article

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,6 +12,9 @@ const navbar = () =>{
     <li id="github" >
       <a href="https://github.com/oslabs-beta/graphQLock" target="_blank" class="button">GitHub</a>
     </li>
+    <li id="medium" >
+      <a href="https://medium.com/@drakeocinco/graphqlock-secure-your-graphql-application-with-ease-e7bc3b85baaa" target="_blank" class="button">Medium Article</a>
+    </li>
   </ul>
   );
 };
